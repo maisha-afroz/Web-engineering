06ed9de4d2f7ce751f403ce8c29640d57203a791
maisha-afroz <63470047+maisha-afroz@users.noreply.github.com>
2020-04-11 05:15:01 +0600

    Web 2nd assignment

---
+++ Assignment 2/Assignment 2-web/javascript/question-generator.js
@@ -0,0 +1,52 @@
+var questions = [
+  {
+    q : "Which of these is not a character from FRIENDS?",
+    o : [
+      "Monica",
+      "John Snow",
+      "Rachel",
+      "Joey"
+    ],
+    a : 1 // arrays start with 0, so it is 70 meters
+  },
+  {
+    q : "What does Joe mean?",
+    o : [
+      "Tea",
+      "Matcha",
+      "Sugar",
+      "Coffee"
+    ],
+    a : 3
+  },
+  {
+    q : "Who won oscar in 2019?",
+    o : [
+      "Carly Jay Repson",
+      "Mark Raffalo",
+      "Jay Hart",
+      "Leonardo De Caprio"
+    ],
+    a : 2
+  },
+  {
+    q : "Which temparature defines normal human body?",
+    o : [
+      "98 F",
+      "100 F",
+      "30 F",
+      "98 C"
+    ],
+    a : 0
+  },
+  {
+    q : "Which movie won the oscar 2020?",
+    o : [
+      "Jumanji: Into the jungle",
+      "Avengers End Game",
+      "Ocean 11",
+      "Parasite"
+    ],
+    a : 3
+  }
+];
